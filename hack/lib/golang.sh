47c3db18bfa81cde78d2ef3dd4993db4d11879d9
Tim Hockin <thockin@google.com>
2022-01-06 12:12:15 -0800

    `make` works
    before:
    1089.39user 99.06system 4:46.13elapsed 415%CPU
    
    after:
    201.30user 42.34system 1:30.08elapsed 270%CPU
    
    I don't know what the major perf diff is yet

--- hack/lib/golang.sh
+++ hack/lib/golang.sh
@@ -78,9 +78,9 @@ kube::golang::server_targets() {
     cmd/kubelet
     cmd/kubeadm
     cmd/kube-scheduler
-    vendor/k8s.io/component-base/logs/kube-log-runner
-    vendor/k8s.io/kube-aggregator
-    vendor/k8s.io/apiextensions-apiserver
+    staging/src/k8s.io/component-base/logs/kube-log-runner
+    staging/src/k8s.io/kube-aggregator
+    staging/src/k8s.io/apiextensions-apiserver
     cluster/gce/gci/mounter
   )
   echo "${targets[@]}"
@@ -127,7 +127,7 @@ kube::golang::node_targets() {
     cmd/kube-proxy
     cmd/kubeadm
     cmd/kubelet
-    vendor/k8s.io/component-base/logs/kube-log-runner
+    staging/src/k8s.io/component-base/logs/kube-log-runner
   )
   echo "${targets[@]}"
 }
@@ -757,7 +757,7 @@ kube::golang::build_some_binaries() {
 
         go test -c -o "$(kube::golang::outfile_for_binary "${package}" "${platform}")" \
           -covermode count \
-          -coverpkg k8s.io/...,k8s.io/kubernetes/vendor/k8s.io/... \
+          -coverpkg k8s.io/... \
           "${build_args[@]}" \
           -tags coverage \
           "${package}"
