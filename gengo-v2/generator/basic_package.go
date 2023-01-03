bd8dfba9177a3a2d122afda42291134801715ddf
Tim Hockin <thockin@google.com>
2023-01-02 20:53:46 -0800

    Archetype rename DefaultPackage

--- gengo-v2/generator/basic_package.go
+++ gengo-v2/generator/basic_package.go
@@ -20,8 +20,10 @@ import (
 	"k8s.io/gengo/v2/types"
 )
 
-// DefaultPackage contains a default implementation of Package.
-type DefaultPackage struct {
+// BasicPackage contains a simple implementation of Package.  It can be used
+// on its own or serve as a base for more specialized Package implementations,
+// by embedding and overriding methods.
+type BasicPackage struct {
 	// Short name of package, used in the "package xxxx" line.
 	PackageName string
 	// Import path of the package, and the location on disk of the package.
@@ -46,25 +48,25 @@ type DefaultPackage struct {
 	FilterFunc func(*Context, *types.Type) bool
 }
 
-func (d *DefaultPackage) Name() string       { return d.PackageName }
-func (d *DefaultPackage) Path() string       { return d.PackagePath }
-func (d *DefaultPackage) SourcePath() string { return d.Source }
+func (d *BasicPackage) Name() string       { return d.PackageName }
+func (d *BasicPackage) Path() string       { return d.PackagePath }
+func (d *BasicPackage) SourcePath() string { return d.Source }
 
-func (d *DefaultPackage) Filter(c *Context, t *types.Type) bool {
+func (d *BasicPackage) Filter(c *Context, t *types.Type) bool {
 	if d.FilterFunc != nil {
 		return d.FilterFunc(c, t)
 	}
 	return true
 }
 
-func (d *DefaultPackage) Generators(c *Context) []Generator {
+func (d *BasicPackage) Generators(c *Context) []Generator {
 	if d.GeneratorFunc != nil {
 		return d.GeneratorFunc(c)
 	}
 	return d.GeneratorList
 }
 
-func (d *DefaultPackage) Header(filename string) []byte {
+func (d *BasicPackage) Header(filename string) []byte {
 	if filename == "doc.go" {
 		return append(d.HeaderText, d.PackageDocumentation...)
 	}
@@ -72,5 +74,5 @@ func (d *DefaultPackage) Header(filename string) []byte {
 }
 
 var (
-	_ = Package(&DefaultPackage{})
+	_ Package = &BasicPackage{}
 )
