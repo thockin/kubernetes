db42b898010d8dacb1cc3aefe30804943494512a
Tim Hockin <thockin@google.com>
2025-03-28 12:04:19 -0700

    Add DNS subdomain format

--- staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/format/zz_generated.validations.go
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/format/zz_generated.validations.go
@@ -53,6 +53,13 @@ func Validate_DNSLabelStringType(ctx context.Context, op operation.Operation, fl
 	return errs
 }
 
+func Validate_DNSSubdomainStringType(ctx context.Context, op operation.Operation, fldPath *field.Path, obj, oldObj *DNSSubdomainStringType) (errs field.ErrorList) {
+	// type DNSSubdomainStringType
+	errs = append(errs, validate.DNSSubdomain(ctx, op, fldPath, obj, oldObj)...)
+
+	return errs
+}
+
 func Validate_IPStringType(ctx context.Context, op operation.Operation, fldPath *field.Path, obj, oldObj *IPStringType) (errs field.ErrorList) {
 	// type IPStringType
 	errs = append(errs, validate.IPSloppy(ctx, op, fldPath, obj, oldObj)...)
@@ -104,6 +111,27 @@ func Validate_Struct(ctx context.Context, op operation.Operation, fldPath *field
 			errs = append(errs, Validate_DNSLabelStringType(ctx, op, fldPath, obj, oldObj)...)
 			return
 		}(fldPath.Child("dnsLabelTypedefField"), &obj.DNSLabelTypedefField, safe.Field(oldObj, func(oldObj *Struct) *DNSLabelStringType { return &oldObj.DNSLabelTypedefField }))...)
+
+	// field Struct.DNSSubdomainField
+	errs = append(errs,
+		func(fldPath *field.Path, obj, oldObj *string) (errs field.ErrorList) {
+			errs = append(errs, validate.DNSSubdomain(ctx, op, fldPath, obj, oldObj)...)
+			return
+		}(fldPath.Child("dnsSubdomainField"), &obj.DNSSubdomainField, safe.Field(oldObj, func(oldObj *Struct) *string { return &oldObj.DNSSubdomainField }))...)
+
+	// field Struct.DNSSubdomainPtrField
+	errs = append(errs,
+		func(fldPath *field.Path, obj, oldObj *string) (errs field.ErrorList) {
+			errs = append(errs, validate.DNSSubdomain(ctx, op, fldPath, obj, oldObj)...)
+			return
+		}(fldPath.Child("dnsSubdomainPtrField"), obj.DNSSubdomainPtrField, safe.Field(oldObj, func(oldObj *Struct) *string { return oldObj.DNSSubdomainPtrField }))...)
+
+	// field Struct.DNSSubdomainTypedefField
+	errs = append(errs,
+		func(fldPath *field.Path, obj, oldObj *DNSSubdomainStringType) (errs field.ErrorList) {
+			errs = append(errs, Validate_DNSSubdomainStringType(ctx, op, fldPath, obj, oldObj)...)
+			return
+		}(fldPath.Child("dnsSubdomainTypedefField"), &obj.DNSSubdomainTypedefField, safe.Field(oldObj, func(oldObj *Struct) *DNSSubdomainStringType { return &oldObj.DNSSubdomainTypedefField }))...)
 
 	return errs
 }
