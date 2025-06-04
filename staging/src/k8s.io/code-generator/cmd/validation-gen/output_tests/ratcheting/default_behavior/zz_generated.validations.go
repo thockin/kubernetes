d594ca2f8a56ed7a1dda0fd6e6d6b6033c814b0a
Tim Hockin <thockin@google.com>
2025-06-03 17:39:05 -0700

    Fix field path for embedded fields in root types
    Given the following:
    
    ```
    type Struct type {
        TypeMeta int
    
        // +k8s:validateFalse="embedded"
        OtherStruct
    }
    ```
    
    What should the field-path be for the error?
    
    or:
    
    ```
    type Struct type {
        TypeMeta int
    
        OtherStruct // embedded
    }
    
    // +k8s:validateFalse="otherstruct"
    type OtherStruct {
      I int
    }
    ```
    
    Conclusion: If we find an embedded field, we look for a nil fldPath, and
    if so we set it to the type name.  Embedded values in root types (e.g.
    above `spec`) should not be a things we actually do.
    
    Implementation: Add `safe.Value(*T, func() *T)`

--- staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/ratcheting/default_behavior/zz_generated.validations.go
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/ratcheting/default_behavior/zz_generated.validations.go
@@ -141,7 +141,7 @@ func Validate_NestedDirectComparableStruct(ctx context.Context, op operation.Ope
 			if op.Type == operation.Update && (obj == oldObj || (obj != nil && oldObj != nil && *obj == *oldObj)) {
 				return nil // no changes
 			}
-			errs = append(errs, validate.FixedResult(ctx, op, fldPath, obj, oldObj, false, "field intField")...)
+			errs = append(errs, validate.FixedResult(ctx, op, fldPath, obj, oldObj, false, "field directComparableStructField")...)
 			errs = append(errs, Validate_DirectComparableStruct(ctx, op, fldPath, obj, oldObj)...)
 			return
 		}(fldPath.Child("directComparableStructField"), &obj.DirectComparableStructField, safe.Field(oldObj, func(oldObj *NestedDirectComparableStruct) *DirectComparableStruct {
@@ -164,7 +164,7 @@ func Validate_NestedNonDirectComparableStruct(ctx context.Context, op operation.
 			if op.Type == operation.Update && equality.Semantic.DeepEqual(obj, oldObj) {
 				return nil // no changes
 			}
-			errs = append(errs, validate.FixedResult(ctx, op, fldPath, obj, oldObj, false, "field intField")...)
+			errs = append(errs, validate.FixedResult(ctx, op, fldPath, obj, oldObj, false, "field nonDirectComparableStructField")...)
 			errs = append(errs, Validate_NonDirectComparableStruct(ctx, op, fldPath, obj, oldObj)...)
 			return
 		}(fldPath.Child("nonDirectComparableStructField"), &obj.NonDirectComparableStructField, safe.Field(oldObj, func(oldObj *NestedNonDirectComparableStruct) *NonDirectComparableStruct {
@@ -187,7 +187,7 @@ func Validate_NonDirectComparableStruct(ctx context.Context, op operation.Operat
 			if op.Type == operation.Update && (obj == oldObj || (obj != nil && oldObj != nil && *obj == *oldObj)) {
 				return nil // no changes
 			}
-			errs = append(errs, validate.FixedResult(ctx, op, fldPath, obj, oldObj, false, "field intField")...)
+			errs = append(errs, validate.FixedResult(ctx, op, fldPath, obj, oldObj, false, "field intPtrField")...)
 			return
 		}(fldPath.Child("intPtrField"), obj.IntPtrField, safe.Field(oldObj, func(oldObj *NonDirectComparableStruct) *int { return oldObj.IntPtrField }))...)
 
