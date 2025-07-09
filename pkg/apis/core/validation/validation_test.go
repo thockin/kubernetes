c258ac0e56380bb207a0641c0c68ae50f8089163
Lalit Chauhan <lalitchauhan@google.com>
2025-07-09 21:41:38 +0000

    refactor(validation): Rename k8s-ip-sloppy format to k8s-ip
    The custom IP address validation format used within Kubernetes was
    previously named 'k8s-ip-sloppy'. This name was chosen as a temporary
    placeholder to reserve the 'k8s-ip' format name for a future, stricter
    IP validation implementation.
    
    With the introduction of validation ratcheting, it is now possible to
    evolve validation rules for fields without breaking existing stored
    objects. This makes the "sloppy" suffix redundant, as the validation
    for 'k8s-ip' can be tightened over time as needed.

--- pkg/apis/core/validation/validation_test.go
+++ pkg/apis/core/validation/validation_test.go
@@ -22093,7 +22093,7 @@ func TestValidateEndpointsCreate(t *testing.T) {
 				}},
 			},
 			expectedErrs: field.ErrorList{
-				field.Invalid(field.NewPath("subsets[0].addresses[0].ip"), nil, "").WithOrigin("format=k8s-ip-sloppy"),
+				field.Invalid(field.NewPath("subsets[0].addresses[0].ip"), nil, "").WithOrigin("format=k8s-ip"),
 			},
 		},
 		"invalid legacy IP with strict validation": {
@@ -22105,7 +22105,7 @@ func TestValidateEndpointsCreate(t *testing.T) {
 				}},
 			},
 			expectedErrs: field.ErrorList{
-				field.Invalid(field.NewPath("subsets[0].addresses[0].ip"), nil, "").WithOrigin("format=k8s-ip-sloppy"),
+				field.Invalid(field.NewPath("subsets[0].addresses[0].ip"), nil, "").WithOrigin("format=k8s-ip"),
 			},
 		},
 		"Multiple ports, one without name": {
@@ -22153,7 +22153,7 @@ func TestValidateEndpointsCreate(t *testing.T) {
 				}},
 			},
 			expectedErrs: field.ErrorList{
-				field.Invalid(field.NewPath("subsets[0].addresses[0].ip"), nil, "").WithOrigin("format=k8s-ip-sloppy"),
+				field.Invalid(field.NewPath("subsets[0].addresses[0].ip"), nil, "").WithOrigin("format=k8s-ip"),
 			},
 		},
 		"Port missing number": {
